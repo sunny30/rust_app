cd3dbf73dfdc8184cdccc65d1f16ea455db4835a
Sharad Singh <sharadsingh8@gmail.com>
2026-01-15 15:53:11 +0530

    again learning rust
    again learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod utils;
-
+use crate::utils::generic_string::str_utils::get_length ;
 fn main() {
+    let s = String::from("Hello World") ;
+    println!("{}", get_length(&s)) ;
     println!("Hello, world!");
 }
---
+++ src/utils/generic_string/str_utils.rs
@@ -0,0 +1,5 @@
+use std::fmt::Display;
+
+pub fn get_length<'a, T:PartialOrd+Display+PartialEq>(s:&'a T) ->usize{ 
+    return s.to_string().len() ;
+}
