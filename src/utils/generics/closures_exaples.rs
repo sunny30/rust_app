e9ee09398f1e93b4ae54eb40732f9036d274df8d
Sharad Singh <sharadsingh8@gmail.com>
2026-02-03 03:30:32 +0530

    generic closure function as an input to other function
    generic closure function as an input to other function

--- src/utils/generics/closures_exaples.rs
+++ src/utils/generics/closures_exaples.rs
@@ -4,3 +4,9 @@ use std::sync::LazyLock;
 static DOT_PRODUCT: LazyLock<Box<dyn for<'a> Fn(&'a Vec<f64>, &'a Vec<f64>) -> f64 + Send + Sync>> = LazyLock::new(|| {
     Box::new(|a, b| a.iter().zip(b.iter()).map(|(&x, &y)| x * y).sum())
 });
+
+
+fn do_twice<T:Add<Output=T>+Mul<Output=T>+Sub<Output=T>+Sized+Copy+Clone, U:Fn(T)->T>(f:U, arg:T) ->T {
+    return  f(arg)+f(arg) ;
+    
+}
