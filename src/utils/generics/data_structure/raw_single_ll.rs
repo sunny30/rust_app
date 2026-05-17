0973d1acf29f32ea2ab9afc1e960b6cc25757ae6
Sharad Singh <sharadsingh8@gmail.com>
2026-05-18 01:47:12 +0530

    single link list with raw pointer structures
    single link list with raw pointer structures

---
+++ src/utils/generics/data_structure/raw_single_ll.rs
@@ -0,0 +1,32 @@
+use std::ptr;
+pub struct SingleLNode<T: ?Sized>{
+    pub data: Box<T>,
+    next: *mut SingleLNode<T>
+}
+
+
+impl<T: ?Sized> SingleLNode<T>{
+    fn new(data: Box<T>)-> *mut Self{
+        Box::into_raw(Box::new(SingleLNode{
+            data: data,
+            next: ptr::null_mut()
+            
+        }))
+    }
+}
+
+pub struct SingleLinkedList<T: ?Sized>{
+    len: i32,
+    head: *mut SingleLNode<T>,
+    tail: *mut SingleLNode<T>
+}
+
+impl<T: ?Sized> SingleLinkedList<T>{
+    fn new()-> Self{
+        SingleLinkedList{
+            len:0,
+            head: ptr::null_mut(),
+            tail: ptr::null_mut()
+        }
+    }
+}
--- src/utils/generics/mod.rs
+++ src/utils/generics/mod.rs
@@ -7,3 +7,4 @@ pub mod sort;
 pub mod state;
 mod advance_closure_function;
 pub mod declarative_macro;
+mod data_structure;
