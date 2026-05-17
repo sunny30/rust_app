9b33a052b9999473f18cb719fc7b5e24ab9dd325
Sharad Singh <sharadsingh8@gmail.com>
2026-05-18 04:47:24 +0530

    bug fixes of previous code
    bug fixes of previous code

--- src/utils/generics/data_structure/raw_single_ll.rs
+++ src/utils/generics/data_structure/raw_single_ll.rs
@@ -16,7 +16,7 @@ impl<T: ?Sized> SingleLNode<T>{
 }
 
 pub struct SingleLinkedList<T: ?Sized>{
-    len: i32,
+    len:  i32,
     head: *mut SingleLNode<T>,
     tail: *mut SingleLNode<T>
 }
@@ -35,10 +35,51 @@ pub fn add_at_tail<'a, T:?Sized>(linklist: &'a mut SingleLinkedList<T>, data:Box
     let tailNode = linklist.tail ;
     let newNode = SingleLNode::new(data) ;
     unsafe {
-        (*tailNode).next = newNode;
-        (*linklist).tail = newNode ;
+        if(tailNode.is_null()){
+            (*linklist).head = newNode;
+            (*linklist).tail = newNode;
+            (*linklist).len = (*linklist).len + 1;
+        }else {
+            (*tailNode).next = newNode;
+            (*linklist).tail = newNode;
+            (*linklist).len = (*linklist).len + 1;
+        }
     }
     
+}
+
+pub fn add_at_head<'a, T:?Sized>(linklist: &'a mut SingleLinkedList<T>, data:Box<T>)->(){
+    let mut headNode = linklist.head ;
+    let mut newNode = SingleLNode::new(data) ;
+
+    unsafe {
+        if(headNode.is_null()){
+            (*linklist).head = newNode;
+            (*linklist).tail = newNode;
+            (*linklist).len = (*linklist).len + 1;
+        }else {
+            (*newNode).next = headNode ;
+            (*linklist).head = newNode;
+            (*linklist).len = (*linklist).len + 1;
+        }
+    }
+}
+
+
+#[test]
+fn single_link_list_test(){
+    let mut list:SingleLinkedList<String> = SingleLinkedList::new() ;
+    add_at_head(&mut list, Box::new(String::from("fist"))) ;
+    add_at_head(&mut list, Box::new(String::from("new_fist"))) ;
+    add_at_tail(&mut list, Box::new(String::from("tail"))) ;
+    add_at_tail(&mut list, Box::new(String::from("new_tail"))) ;
+    let mut curr_head = list.head ;
+    for i in 0..list.len{
+        unsafe { 
+            println!("{:?}", (*curr_head).data); 
+            curr_head = (*curr_head).next ;
+        }
+    }
 }
 
 
