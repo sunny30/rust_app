f8785c9d5d037cf3e31bae4f0d488b445e8412c3
Sharad Singh <sharadsingh8@gmail.com>
2026-05-20 19:39:28 +0530

    drop at index worked
    drop at index worked

--- src/utils/generics/data_structure/raw_single_ll.rs
+++ src/utils/generics/data_structure/raw_single_ll.rs
@@ -81,6 +81,32 @@ pub fn get_node_at_index<'a, T:?Sized>(linklist: &'a mut SingleLinkedList<T>, in
 }
 
 
+pub fn drop_at_index<'a, T:?Sized>(linklist: &'a mut SingleLinkedList<T>, index:i32)->(){
+    if index == 0 {
+        let mut headNode = linklist.head ;
+        unsafe {
+            let mut nextNode = (*headNode).next;
+            headNode = ptr::null_mut() ;
+            linklist.head = nextNode ;
+        }
+        
+    }else{
+        let mut priorNode = get_node_at_index(linklist,index-1) ;
+        unsafe {
+            let mut indexNode = (*priorNode).next ;
+            let mut nextNode = (*((*priorNode).next)).next ;
+            (*priorNode).next = nextNode ;
+            (*indexNode).next = ptr::null_mut() ;
+            
+            if(index == linklist.len-1){
+                linklist.tail = priorNode ;
+            }
+        }
+    }
+    linklist.len-=1 ;
+}
+
+
 #[test]
 fn single_link_list_test(){
     let mut list:SingleLinkedList<String> = SingleLinkedList::new() ;
@@ -88,6 +114,8 @@ fn single_link_list_test(){
     add_at_head(&mut list, Box::new(String::from("new_fist"))) ;
     add_at_tail(&mut list, Box::new(String::from("tail"))) ;
     add_at_tail(&mut list, Box::new(String::from("new_tail"))) ;
+    
+    drop_at_index(&mut list, 3) ;
     let mut curr_head = list.head ;
     unsafe {
         for i in 0..list.len {
@@ -95,8 +123,8 @@ fn single_link_list_test(){
             curr_head = (*curr_head).next;
         }
 
-        println!("{:?}", (*get_node_at_index(&mut list, 2)).data);
-        println!("{:?}", (*get_node_at_index(&mut list, 0)).data)
+       // println!("{:?}", (*get_node_at_index(&mut list, 2)).data);
+       // println!("{:?}", (*get_node_at_index(&mut list, 0)).data)
     }
 }
 
