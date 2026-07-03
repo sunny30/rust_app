22bbc45e5f87aa2845ae7ccb0ad27941efb5e879
Sharad Singh <sharadsingh8@gmail.com>
2026-07-03 16:18:47 +0530

    Shared mut stitch to Ref and Ref_Mut
    Shared mut stitch to Ref and Ref_Mut

--- src/utils/generics/data_structure/safe_rc_mut.rs
+++ src/utils/generics/data_structure/safe_rc_mut.rs
@@ -1,6 +1,7 @@
 use std::cell::UnsafeCell;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
+use crate::utils::generics::priority_queue_struct::Element;
 
 struct Inner<T>{
     ref_count: i32,
@@ -24,6 +25,26 @@ pub struct SharedMut<T>{
     ptr: NonNull<Inner<T>>
 }
 
+impl<T> SharedMut<T>{
+    pub fn new(&self, value:T)->Self{
+        let inner = Inner::new(value) ;
+        let boxed = Box::new(inner) ;
+        let raw_ptr = Box::into_raw(boxed) ;
+        SharedMut{
+            ptr: unsafe{NonNull::new_unchecked(raw_ptr)},
+            _marker:PhantomData
+        }
+    }
+}
 
+
+pub struct Ref<'a, T> {
+    inner: &'a Inner<T>,
+}
+
+
+pub struct RefMut<'a, T> {
+    inner: &'a Inner<T>,
+}
 
 
