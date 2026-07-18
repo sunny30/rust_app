8ab34d7fe6ada79bcddeec57707c76d14dc52842
Sharad Singh <sharadsingh8@gmail.com>
2026-07-18 11:20:35 +0530

    SharedMut Rust borrow, borrow_mut and Clone
    SharedMut Rust borrow, borrow_mut and Clone

--- src/utils/generics/data_structure/safe_rc_mut.rs
+++ src/utils/generics/data_structure/safe_rc_mut.rs
@@ -35,6 +35,50 @@ impl<T> SharedMut<T>{
             _marker:PhantomData
         }
     }
+
+
+
+    /// Get a shared (immutable) borrow of the inner value.
+    /// Panics if the value is currently mutably borrowed.
+    pub fn borrow(&self) -> Ref<'_, T> {
+        let inner = unsafe { self.ptr.as_ref() };
+
+        if inner.borrow_state < 0 {
+            panic!("SharedMut: already mutably borrowed");
+        }
+
+        // Safety: we just checked it's not -1, safe to increment
+        unsafe {
+            (*self.ptr.as_ptr()).borrow_state += 1;
+        }
+
+        Ref { inner }
+    }
+
+    /// Get an exclusive (mutable) borrow of the inner value.
+    /// Panics if the value is currently borrowed in any way.
+    pub fn borrow_mut(&self) -> RefMut<'_, T> {
+        let inner = unsafe { self.ptr.as_ref() };
+
+        if inner.borrow_state != 0 {
+            panic!(
+                "SharedMut: already borrowed (borrow_state = {})",
+                inner.borrow_state
+            );
+        }
+
+        // Safety: we just checked it's 0, safe to set -1
+        unsafe {
+            (*self.ptr.as_ptr()).borrow_state = -1;
+        }
+
+        RefMut { inner }
+    }
+
+    /// How many SharedMut handles point to this value right now.
+    pub fn ref_count(&self) -> i32 {
+        unsafe { self.ptr.as_ref().ref_count }
+    }
 }
 
 
@@ -46,5 +90,21 @@ pub struct Ref<'a, T> {
 pub struct RefMut<'a, T> {
     inner: &'a Inner<T>,
 }
+
+
+impl<T> Clone for SharedMut<T>{
+    fn clone(&self) -> Self {
+        unsafe {
+            (*(self.ptr.as_ptr())).ref_count += 1; //reason it is working
+            //NotNull return returns *mut Inner<T>, NonNull::as_ptr() always returns *mut T
+
+        }
+        SharedMut{
+            ptr: self.ptr,
+            _marker: PhantomData
+        }
+    }
+}
+
 
 
